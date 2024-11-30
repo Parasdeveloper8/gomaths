6c0b5a87e34fc4061d0839a9e1fe4166140acb2d
Paras prajapat <parasprajapat8th.gmail.com>
2024-11-30 13:59:57 +0530

    Readme written

---
+++ README.md
@@ -0,0 +1,7 @@
+# GOMATHS
+- This package can you help you in mathematics 
+- example :- force,acceleration,etc
+
+-You can download this using this link ["github.com/Parasdeveloper8/gomaths.git/physics"]("github.com/Parasdeveloper8/gomaths.git/physics")
+
+##Parasdevloper8
--- physics/force.go
+++ physics/force.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 )
 
+// Force formulae
 func GetForce(mass, acceleration float64) uint {
-	force := mass * acceleration
-	forceUint := uint(force)
+	force := mass * acceleration // f =m x a
+	forceUint := uint(force)     //conversion of float64 force into uint
 	fmt.Printf("Force is %v \n", forceUint)
 	return forceUint
 }
