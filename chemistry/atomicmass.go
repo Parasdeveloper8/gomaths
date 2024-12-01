b749684b31da2a564582ce94f5979624137be5a5
Paras prajapat <parasprajapat8th.gmail.com>
2024-12-01 11:25:21 +0530

    Improved function to get Atomic mass

--- chemistry/atomicmass.go
+++ chemistry/atomicmass.go
@@ -9,31 +9,46 @@ import (
 )
 
 type Element struct {
-	Name string  `json:"name"`
-	Mass float64 `json:"mass"`
+	Name        string  `json:"name"`
+	Atomic_Mass float64 `json:"atomic_mass"`
+	Symbol      string  `json:"symbol"`
 }
 
+// GetAtomicMass retrieves the atomic mass of an element by name or symbol.
 func GetAtomicMass(name string) (float64, bool) {
-	file, err := os.Open("D:/gomaths/json/elements.json")
+
+	// Open the elements file
+	file, err := os.Open("D:/gomaths/json/elements.json") // Path to elements.json
 	if err != nil {
-		fmt.Printf("Couldnot open file %v", err)
+		fmt.Printf("Could not open file: %v\n", err)
+		return 0, false
 	}
 	defer file.Close()
+
+	// Read file contents
 	bytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		fmt.Printf("Could not read file %v", err)
-	}
-	var elements []Element
-	if err := json.Unmarshal(bytes, &elements); err != nil {
-		log.Fatalf("Failed to parse JSON %v", err)
+		fmt.Printf("Could not read file: %v\n", err)
+		return 0, false
 	}
 
-	for _, element := range elements {
-		if element.Name == name {
-			fmt.Printf("Element %v\n : Mass %v \n", name, element.Mass)
-			return element.Mass, true
-		}
-		//fmt.Printf("Element name : %v\n Element mass :%v\n", element.Name, element.Mass)
+	// Parse JSON into slice of elements
+	var elements []Element
+	if err := json.Unmarshal(bytes, &elements); err != nil {
+		log.Fatalf("Failed to parse JSON: %v", err)
+		return 0, false
 	}
+
+	// Search for the element by name or symbol
+	for _, element := range elements {
+		if (len(name) == 2 || len(name) < 2 && name == element.Symbol) || (len(name) > 2 && name == element.Name) {
+			fmt.Printf("Element: %v\n Mass: %v\nSymbol: %v\n", element.Name, element.Atomic_Mass, element.Symbol)
+			return element.Atomic_Mass, true
+		}
+
+	}
+
+	// If element is not found
+	fmt.Println("Element not found.")
 	return 0, false
 }
--- main.go
+++ main.go
@@ -6,5 +6,5 @@ import (
 
 func main() {
 	//physics.ForceGravity(6*math.Pow(10, 24), 7.4*math.Pow(10, 22), 384*math.Pow(10, 8))
-	chemistry.GetAtomicMass("Carbon")
+	chemistry.GetAtomicMass("")
 }
