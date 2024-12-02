e5f4517e01b4c14af09398c26082a5130894e101
Paras prajapat <parasprajapat8th.gmail.com>
2024-12-02 21:09:45 +0530

    Minor fix

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"github.com/Parasdeveloper8/gomaths.git/chemistry"
 	"github.com/Parasdeveloper8/gomaths.git/mathematics"
 )
 
 func main() {
 	//physics.ForceGravity(6*math.Pow(10, 24), 7.4*math.Pow(10, 22), 384*math.Pow(10, 8))
-	//chemistry.GetAtomicMass("")
+	chemistry.GetAtomicMass("O")
 	//mathematics.EnglishToRoman("8")
-	mathematics.GetFormula("")
+	mathematics.GetFormula("Sum of Cubes")
 }
